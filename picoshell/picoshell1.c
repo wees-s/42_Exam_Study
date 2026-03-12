41e48491603eb0494678e3e64588ca13a09658e3
wees-s <wesleyalexandre3946@gmail.com>
2026-03-12 11:34:21 -0300

    update

--- picoshell/picoshell1.c
+++ picoshell/picoshell1.c
@@ -9,27 +9,64 @@ int picoshell(char **cmds[])
     int fd[2];
     int i = 0;
     pid_t pid;
-    /*
-    char **cmds[] = {
-        cmds[0] = char *cmd1[] = {"wc", "-l", NULL};,
-        cmds[1] = char *cmd2[] = {"wc", "-l", NULL};,
-        cmds[2] = NULL
-        };*/
-    while (cmds[i])//array de array de strings
+    int in_file;
+    int prev_fd = -1;
+    while (cmds[i])
     {
-        /*if (cmds[i + 1])
+        //pipe
+        //fork
+        //dup2
+        //execvp
+        if (cmds[i + 1]) //se houver comando ainda para rodar
         {
-            
-        }*/
-        pid = fork();
-        if (pid == 0)
-        {
-            execvp(cmds[i][0], cmds[i]);
+            pipe(fd); //faça o pipe
         }
-        i++;
+        else //caso não tenha um proximo comando para rodar, fd[0] e fd[1] passam a ser negativos (flagzinha)
+        {
+            fd[0] = -1;
+            fd[1] = -1;
+        }
+        pid = fork();
+        if (pid == 0)//to no filho
+        {
+            if (prev_fd != -1)//se não é a primeira vez dentro do pipe
+            {
+                dup2(prev_fd, STDIN_FILENO);
+                close(prev_fd);
+            }
+            if (cmds[i + 1])//se estou no filho e ainda tiver um comando vindo pela frente
+            {
+                dup2(fd[1], STDOUT_FILENO);//A SAÍDA DE ESCRITA PASSA A SER A ENTRADA
+                //trocar a saída 
+            }
+            close(fd[0]);
+            close(fd[1]);
+            execvp(cmds[i][0], cmds[i]);
+            exit(1);
+            // a execução precisa accontecer aqui especificamente
+        }
+        else if (pid == -1) //erro
+        {
+            if (fd[0] != -1)
+                close(fd[0]);
+            if (fd[1] != -1)
+                close(fd[1]);
+            if (prev_fd != 0)
+                close(prev_fd);
+            exit(1);
+        }
+        else //to no pai
+        {
+            close(fd[1]);
+            if (prev_fd != -1)
+            {
+                close(prev_fd);
+            }
+            prev_fd = fd[0];
+            i++;
+        }
     }
-    ret = 0;
-    return (ret);
+    return 0;
 }
 
 #include <stdio.h>
